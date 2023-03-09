f8a44263a66a31bf811ae18615a2f12190d2bef5
MohammadRahman <45675266+MohammadRahman@users.noreply.github.com>
2023-03-10 00:27:05 +0200

    server data fixed

--- src/service/user.ts
+++ src/service/user.ts
@@ -2,33 +2,35 @@ import { UserDocument, UserInputs, USER_MODEL } from "../models/user";
 import { databaseResponseTimeHistogram } from "../utils/metrics";
 
 export async function createUserService(input: UserInputs) {
-    const metricsLabel = {
-        operation: 'createUserService'
-    }
-    const timer = databaseResponseTimeHistogram.startTimer()
-    try {
-        const result = await USER_MODEL.create(input)
-        timer({ ...metricsLabel,success:'true' })
-        return result
-    } catch (error) {
-        timer({ ...metricsLabel, success: 'true' })
-        return error
-    }
+    // const metricsLabel = {
+    //     operation: 'createUserService'
+    // }
+    // const timer = databaseResponseTimeHistogram.startTimer()
+    // try {
+        // const result =
+           return  await USER_MODEL.create(input)
+        // timer({ ...metricsLabel,success:'true' })
+        // return result
+    // } catch (error) {
+    //     // timer({ ...metricsLabel, success: 'true' })
+    //     return error
+    // }
 }
 export async function findOneUserByEmail(email: UserDocument['email']) {
     
-        const metricsLabel = {
-            operation: 'createUserService'
-        }
-    const timer = databaseResponseTimeHistogram.startTimer()
+    //     const metricsLabel = {
+    //         operation: 'createUserService'
+    //     }
+    // const timer = databaseResponseTimeHistogram.startTimer()
     
-    try {
-        const result = await USER_MODEL.findOne({ email })
-        timer({ ...metricsLabel, success: 'true' })
-        return result
-    } catch (error) {
-        timer({ ...metricsLabel, success: 'true' })
-        return error
-    }
+    // try {
+    // const result =
+       return await USER_MODEL.findOne({ email })
+        // timer({ ...metricsLabel, success: 'true' })
+        // return result
+    // } catch (error) {
+        // timer({ ...metricsLabel, success: 'true' })
+        // return error
+    // }
        
 }
